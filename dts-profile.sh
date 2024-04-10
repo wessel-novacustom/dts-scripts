2583e7e5fa678206964ec8f1c7a1b6816bfa4fb2
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2024-04-10 11:10:52 +0200

    relicense to Apache-2.0, reuse compliance
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- dts-profile.sh
+++ dts-profile.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# SPDX-FileCopyrightText: 2024 3mdeb <contact@3mdeb.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 SBIN_DIR="/usr/sbin"
 export DTS_FUNCS="$SBIN_DIR/dts-functions.sh"
 export DTS_ENV="$SBIN_DIR/dts-environment.sh"
--- include/dts-environment.sh
+++ include/dts-environment.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# SPDX-FileCopyrightText: 2024 3mdeb <contact@3mdeb.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # Text Reset
 COLOR_OFF='\033[0m'
 RED='\033[0;31m'
