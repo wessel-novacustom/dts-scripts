f89f49e67f34da49ede029ba87ea8ca18f18f4bf
Wiktor Grzywacz <wiktor.grzywacz@3mdeb.com>
2024-07-10 12:37:46 +0200

    scripts/local-deploy.sh: added

---
+++ scripts/local-deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This scripts deplos DTS scripts into target machine, for local development
+
+IP_ADDR="$1"
+PORT=${PORT:-22}
+
+scp -P ${PORT} \
+  -O \
+  -o StrictHostKeyChecking=no \
+  -o UserKnownHostsFile=/dev/null \
+  include/* \
+  reports/* \
+  scripts/* \
+  unit_tests/* \
+  dts-profile.sh \
+  root@${IP_ADDR}:/usr/sbin
